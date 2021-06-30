07c06472e4806581319376ad343cfda9ddaa4527
rubenfgr <rubenfgr87@outlook.com>
2021-06-30 13:04:02 +0200

    update link to action sheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,33 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: 'alert',
+    loadChildren: () =>
+      import('./pages/alert/alert.module').then((m) => m.AlertPageModule),
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () =>
+      import('./pages/action-sheet/action-sheet.module').then(
+        (m) => m.ActionSheetPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
